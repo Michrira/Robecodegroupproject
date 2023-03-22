b1264c0b8efcea9f1ccc7524f39497fabe912da3
Belen Andrade <belenandrade2000@gmail.com>
2023-03-22 13:19:29 -0700

    merging

---
+++ script.js
@@ -0,0 +1,19 @@
+
+var happy = [];
+var sad = [];
+var excited = [];
+var angry = [];
+
+
+
+
+var requestURL = "https://api.themoviedb.org/3/discover/movie?api_key=7a5e1c0d82259f941772e1baf73aad08&with_original_language=en&with_genres="
+
+
+
+/*fetch(requestURL)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  console.log(data)

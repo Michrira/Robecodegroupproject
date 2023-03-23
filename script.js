ca574d44ceb749f30e8e957d8eb57a5dcf2decad
Belen Andrade <belenandrade2000@gmail.com>
2023-03-23 11:52:16 -0700

    buttons modified

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 
-var happy = [];
-var sad = [];
-var excited = [];
-var angry = [];
-
-
-
-
-var requestURL = "https://api.themoviedb.org/3/discover/movie?api_key=7a5e1c0d82259f941772e1baf73aad08&with_original_language=en&with_genres="
-
 
 
 /*fetch(requestURL)

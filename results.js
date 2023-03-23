9b9a08e38f3185451591937af060f1a7ca627d2a
Patricia <124640646+P-Trish@users.noreply.github.com>
2023-03-23 14:30:11 -0700

    food options function in results js

--- results.js
+++ results.js
@@ -8,9 +8,9 @@ var showFoodBtn = document.getElementById("showFood");
 // aboutMovieBtn.addEventListener("click", )
 
 function showFoodOptions(showFoodBtn) {
-
+// add yelp code here so that button takes user to the yelp API
 }
 
-showFoodBtn.addEventListener("Click", function());
+showFoodBtn.addEventListener("click", showFoodOptions);
 
-// when the user clicks the 
+// when the user clicks the "Show Me Options" button under food results, they are taken top the yelp page 

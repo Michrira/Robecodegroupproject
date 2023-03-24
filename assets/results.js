0c44c22d9d9f0f8c38897a1599fa59055b0ae38a
Michrira <Michaelchristianrader@gmail.com>
2023-03-24 11:30:09 -0700

    changed some JS and html

--- assets/results.js
+++ assets/results.js
@@ -1,3 +1,20 @@
+// var aboutMovieBtn = document.getElementById("movieInfo");
+var showFoodBtn = document.getElementById("showFood");
+
+// function showMovieInfo (aboutMovieBtn){
+    
+// }
+
+// aboutMovieBtn.addEventListener("click", )
+
+function showFoodOptions(showFoodBtn) {
+// add yelp code here so that button takes user to the yelp API
+}
+
+showFoodBtn.addEventListener("click", showFoodOptions);
+
+// when the user clicks the "Show Me Options" button under food results, they are taken top the yelp page 
+
 var movie = document.querySelector("movie-placeholder");
 var btn = document.querySelector("button")
 

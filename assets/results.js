3aab2051c24256bea7e51f42e6e072ef9f6e9265
shawn220a <shawn.42496@gmail.com>
2023-03-24 20:59:24 -0500

    redirect user

--- assets/results.js
+++ assets/results.js
@@ -1,56 +1,56 @@
-// var aboutMovieBtn = document.getElementById("movieInfo");
-var showFoodBtn = document.getElementById("showFood");
+// // var aboutMovieBtn = document.getElementById("movieInfo");
+// var showFoodBtn = document.getElementById("showFood");
 
-// function showMovieInfo (aboutMovieBtn){
+// // function showMovieInfo (aboutMovieBtn){
 
+// // }
+
+// // aboutMovieBtn.addEventListener("click", )
+
+// function showFoodOptions(showFoodBtn) {
+// // add yelp code here so that button takes user to the yelp API
 // }
 
-// aboutMovieBtn.addEventListener("click", )
-
-function showFoodOptions(showFoodBtn) {
-// add yelp code here so that button takes user to the yelp API
-}
-
-
-showFoodBtn.addEventListener("click", showFoodOptions);
+// showFoodBtn.addEventListener("click", showFoodOptions);
 
 //showFoodBtn.addEventListener("click", showFoodOptions);
 
+// when the user clicks the "Show Me Options" button under food results, they are taken top the yelp page
 
-// when the user clicks the "Show Me Options" button under food results, they are taken top the yelp page 
+// var movie = document.querySelector("movie-placeholder");
+// var btn = document.querySelector("button")
 
-var movie = document.querySelector("movie-placeholder");
-var btn = document.querySelector("button")
+// btn.onclick=function() {
+//     if (movie.style.display=="none") {
+//         movie.style.display== "block"
+//     } else {
+//         movie.style.display=="none"
+//     }
+// }
 
-btn.onclick=function() {
-    if (movie.style.display=="none") {
-        movie.style.display== "block"
-    } else {
-        movie.style.display=="none"
-    }
-}
 
-function displayRandomMovie(){
-    var randomMovie = JSON.parse(localStorage.getItem("random-movie"));
-    console.log(randomMovie)
-    var title = document.createElement('h3');
-    var overview = document.createElement('p');
-    var poster = document.createElement("img")
 
-var movieResultEl = document.querySelector(".moviePicks");
+// function displayRandomMovie() {
+//   var randomMovie = JSON.parse(localStorage.getItem('random-movie'));
+//   console.log(randomMovie);
+//   var title = document.createElement('h3');
+//   var overview = document.createElement('p');
+//   var poster = document.createElement('img');
 
-    title.textContent = randomMovie.title;
-    console.log(title)
-    // overview with click button
-    overview.textContent = randomMovie.overview;
+//   var movieResultEl = document.querySelector('.moviePicks');
 
-    // poster image like this as well? or does image need special elements
-    // will have to concat the poster image with base url
+//   title.textContent = randomMovie.title;
+//   console.log(title);
+//   // overview with click button
+//   overview.textContent = randomMovie.overview;
 
-    var posterurl = "https://image.tmdb.org/t/p/original/"
+//   // poster image like this as well? or does image need special elements
+//   // will have to concat the poster image with base url
 
-    var completePosterUrl = posterurl.concat(randomMovie.poster_path)
-    poster.setAttribute("src",completePosterUrl)
-    movieResultEl.append(title,overview,poster);
-}
-displayRandomMovie();
+//   var posterurl = 'https://image.tmdb.org/t/p/original/';
+
+//   var completePosterUrl = posterurl.concat(randomMovie.poster_path);
+//   poster.setAttribute('src', completePosterUrl);
+//   movieResultEl.append(title, overview, poster);
+// }
+// displayRandomMovie();

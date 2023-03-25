1eace7ebf71d19aeb11d2d9b0f95cb8c094ea70c
shawn220a <shawn.42496@gmail.com>
2023-03-24 21:10:38 -0500

    Clean up code

--- assets/results.js
+++ assets/results.js
@@ -1,33 +1,3 @@
-// // var aboutMovieBtn = document.getElementById("movieInfo");
-// var showFoodBtn = document.getElementById("showFood");
-
-// // function showMovieInfo (aboutMovieBtn){
-
-// // }
-
-// // aboutMovieBtn.addEventListener("click", )
-
-// function showFoodOptions(showFoodBtn) {
-// // add yelp code here so that button takes user to the yelp API
-// }
-
-// showFoodBtn.addEventListener("click", showFoodOptions);
-
-//showFoodBtn.addEventListener("click", showFoodOptions);
-
-// when the user clicks the "Show Me Options" button under food results, they are taken top the yelp page
-
-// var movie = document.querySelector("movie-placeholder");
-// var btn = document.querySelector("button")
-
-// btn.onclick=function() {
-//     if (movie.style.display=="none") {
-//         movie.style.display== "block"
-//     } else {
-//         movie.style.display=="none"
-//     }
-// }
-
 function displayRandomMovie() {
   var randomMovie = JSON.parse(localStorage.getItem('random-movie'));
   console.log(randomMovie);
